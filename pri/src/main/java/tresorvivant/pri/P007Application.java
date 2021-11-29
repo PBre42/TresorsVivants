fc6f2aa5b29463261a39a04754671fe826d99eab
MaximeFeuillet <85481775+MaximeFeuillet@users.noreply.github.com>
2021-11-29 10:09:36 +0100

    Add files via upload

---
+++ pri/src/main/java/tresorvivant/pri/P007Application.java
@@ -0,0 +1,13 @@
+package tresorvivant.pri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P007Application.class, args);
+	}
+
+}
---
+++ pri/src/test/java/tresorvivant/pri/P007ApplicationTests.java
@@ -0,0 +1,13 @@
+package tresorvivant.pri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
